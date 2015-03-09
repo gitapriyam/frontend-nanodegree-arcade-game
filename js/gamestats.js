d6eddfbbe4ec2a536418e920574a56a3d7ad8b84
gitapriyam <gitapriyam@gmail.com>
2015-03-08 23:04:25 -0700

    The functional game is done

---
+++ js/gamestats.js
@@ -0,0 +1,3 @@
+﻿var wealth = window.sessionStorage.getItem("wealth");
+document.getElementById("wealth").innerHTML = "You had a maximum treasure of " + wealth;
+document.getElementById("duration").innerHTML = "Game duration (HH:MI:SS) is " + window.sessionStorage.getItem("duration");
